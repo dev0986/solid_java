9d7259f1dd0ec5621d4b1350bb46da1e3febba38
atish.gurumurthi <atish.gurumurthi@gmail.com>
2023-05-08 12:52:31 -0400

    Updated Main.java Single responsibility

--- src/main/java/org/example/single/responsibility/Main.java
+++ src/main/java/org/example/single/responsibility/Main.java
@@ -1,4 +1,33 @@
 package org.example.single.responsibility;
 
+import org.example.single.responsibility.Account;
+import org.example.single.responsibility.AccountOperations;
+import org.example.single.responsibility.TransactionsOperation;
+
+import java.math.BigDecimal;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+
+    public static void main(String[] args) {
+        AccountOperations accountOperations = new AccountOperations();
+        TransactionsOperation transactionsOperation = new TransactionsOperation();
+
+        Account account1 = new Account("Atish", 1111, new BigDecimal("100.00"));
+        Account account2 = new Account("Disha", 2222, new BigDecimal("200.00"));
+        Account account3 = new Account("Shruti", 3333, new BigDecimal("300.00"));
+
+        accountOperations.addAccount(account1);
+        accountOperations.addAccount(account2);
+        accountOperations.addAccount(account3);
+
+        transactionsOperation.deposit(account1, BigDecimal.TEN);
+
+        accountOperations.displayAllAccounts().forEach( (account) -> System.out.println(account));
+
+
+
+    }
 }
