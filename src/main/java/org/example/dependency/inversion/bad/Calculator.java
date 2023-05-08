611298a567c31b6c4cbfeb817165886290f5916b
atish.gurumurthi <atish.gurumurthi@gmail.com>
2023-05-08 13:06:56 -0400

    D'Inversion DCB SOLID 2

--- src/main/java/org/example/dependency/inversion/bad/Calculator.java
+++ src/main/java/org/example/dependency/inversion/bad/Calculator.java
@@ -9,11 +9,16 @@ public class Calculator {
         switch (operation) {
             case "add":
                 System.out.println("Add");
+
+                //Hard coding dependency here.
+                //Higher module - calculate depending on AddOperation
                 AddOperation addOperation = new AddOperation();
                 result  = addOperation.add(a,b);
                 break;
             case "sub":
                 System.out.println("Subtract");
+                //Hard coding dependency here.
+                //Higher module - calculate depending on SubOperation
                 SubOperation subOperation = new SubOperation();
                 result = subOperation.subtract(a,b);
                 break;
